ee7bc5d9ea30f9864fbed25efc83fe60b7f89805
yzddd04 <ahmadyazidarifuddin04@gmail.com>
2025-09-13 22:38:01 +0700

    feat: tambah icon tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Note App')</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <style>
         * {
             margin: 0;
